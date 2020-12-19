115473d4a3c6e3bf76e1b674c5a66e105e6e1748
Wlad Wagner <64201310+wlad@users.noreply.github.com>
2020-12-19 20:17:54 +0100

    delete all [skip ci]

--- src/main/java/org/ehrbase/fhirbridge/FhirBridgeException.java
+++
@@ -1,8 +0,0 @@
-package org.ehrbase.fhirbridge;
-
-public class FhirBridgeException extends RuntimeException {
-
-    public FhirBridgeException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
